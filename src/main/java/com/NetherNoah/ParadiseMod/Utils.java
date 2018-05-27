2b46f5f97bd28a803171d423d9f8c84f322b207c
Noah Jelen <noahjelen@icloud.com>
2018-05-26 21:53:53 -0700

    Forgot something

--- src/main/java/com/NetherNoah/ParadiseMod/Utils.java
+++ src/main/java/com/NetherNoah/ParadiseMod/Utils.java
@@ -8,13 +8,18 @@ import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class Utils {
+	/**
+	 *Registers a block
+	 */
 	public static void regBlock(Block block) {
 		ForgeRegistries.BLOCKS.register(block);
 		ItemBlock item = new ItemBlock(block);
 		item.setRegistryName(block.getRegistryName());
 		ForgeRegistries.ITEMS.register(item);
 	}
-
+	/**
+	 *Registers an item model
+	 */
 	public static void regRender(Block block) {
 		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0,
 				new ModelResourceLocation(block.getRegistryName(),"inventory"));
